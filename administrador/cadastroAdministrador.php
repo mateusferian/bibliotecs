d2e59b9cd8e745b797907314b4f3d084a321bd45
mateusferian <mateusferian10@gmail.com>
2023-10-27 09:17:33 -0300

    :recycle: refactor: refactor  titles

--- administrador/cadastroAdministrador.php
+++ administrador/cadastroAdministrador.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Administrador</title>
+    <title>Bibliotecs</title>
 
     <link href="../bootstrap/bootstrapCSS/bootstrap.min.css" rel="stylesheet">
     <script src="../bootstrap/bootstrapJS/bootstrap.min.js"> </script>
